677baf6d8f0511b8d8a7f0733cad6b9efc90ddeb
Jesus Ortiz <jesusdortiz18@gmail.com>
2025-05-03 13:06:39 +0200

    Last api fix

--- src/api/anouncementApi.ts
+++ src/api/anouncementApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:8080/api/announcement";
-const API_URL = "https://hostalmanagementbackend.onrender.com/announcement";
+const API_URL = "https://hostalmanagementbackend.onrender.com/api/announcement";
 
 // Define TypeScript interfaces for your data
 interface Announcement {
