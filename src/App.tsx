afe94f24d85b2a27e95fbf67a546b29806a5e3b3
Jesus Ortiz <jesusdortiz18@gmail.com>
2025-05-14 13:53:49 +0200

    Merging all changes into development

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes"; // We'll define this next
 import { UserProvider } from "./components/auth/UserContext";
-import CheckInOut from "./pages/CheckInOut";
-import NotFound from "./components/ui/NotFound";
-import Finances from "./pages/Finances";
 
 function App() {
   return (
     <Router>
       <UserProvider>
-
-      <div className="flex h-screen">
-
-        {/* Sidebar */}
-        <Sidebar />
-        {/* Main Content */}
-        <div className="flex-1 p-5 dark:bg-gray-900 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-800">
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/login" element={<LoginPage />} />
-
-            {/* Standard Routes */}
-            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-            <Route path="/guests" element={<ProtectedRoute><Guests /></ProtectedRoute>} />
-            <Route path="/checkins" element={<ProtectedRoute><CheckInOut /></ProtectedRoute>} />
-            <Route path="/reservations" element={<ProtectedRoute><Reservations /></ProtectedRoute>} />
-            <Route path="/rooms" element={<ProtectedRoute><Rooms/></ProtectedRoute>} />
-            <Route path="/announcements" element={<ProtectedRoute><Announcements /></ProtectedRoute>} />
-            <Route path="/users-overview" element={<ProtectedRoute><Users/></ProtectedRoute>} />
-            <Route path="/finances" element={<ProtectedRoute><Finances/></ProtectedRoute>} />
-
-            {/* Creation Routes - Protected */}
-            <Route path="/create-user" element={<ProtectedRoute><CreateUserForm /></ProtectedRoute>} />
-            <Route path="/create-announcement" element={<ProtectedRoute><CreateAnouncementForm /></ProtectedRoute>} />
-            <Route path="/create-room" element={<ProtectedRoute><CreateRoomForm /></ProtectedRoute>} />
-            <Route path="/create-reservation" element={<ProtectedRoute><CreateReservationForm /></ProtectedRoute>} />
-
-            {/* Delete - Protected */}
-            {/* Fallback route */}
-            <Route path="*" element={<ProtectedRoute><NotFound /></ProtectedRoute>} />
-          </Routes>
-
-        </div>
-      </div>
-          </ UserProvider>
+        <AppRoutes />
+      </UserProvider>
     </Router>
   );
 }
--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -14,6 +14,8 @@ import CreateRoomForm from "./components/forms/CreateRoomForm";
 import CreateReservationForm from "./components/forms/CreateReservationForm";
 import LoginPage from "./components/auth/LoginForm";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Finances from "./pages/Finances";
+import CheckInOut from "./pages/CheckInOut";
 
 const AppRoutes = () => {
   const { isAuthenticated } = useUser();
@@ -33,10 +35,12 @@ const AppRoutes = () => {
           <Route path="/" element={isAuthenticated ? <ProtectedRoute><Dashboard /></ProtectedRoute> : <LoginPage />}/>
           <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           <Route path="/guests" element={<ProtectedRoute><Guests /></ProtectedRoute>} />
-          <Route path="/reservations" element={<ProtectedRoute><Reservations /></ProtectedRoute>} />
+          <Route path="/checkins" element={<ProtectedRoute><CheckInOut /></ProtectedRoute>} />
+          <Route path="/reservations" element={<ProtectedRoute><Reservations /></ProtectedRoute>} />          
           <Route path="/rooms" element={<ProtectedRoute><Rooms /></ProtectedRoute>} />
           <Route path="/announcements" element={<ProtectedRoute><Announcements /></ProtectedRoute>} />
-          <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
+          <Route path="/users-overview" element={<ProtectedRoute><Users/></ProtectedRoute>} />
+          <Route path="/finances" element={<ProtectedRoute><Finances/></ProtectedRoute>} />          
           <Route path="/create-user" element={<ProtectedRoute><CreateUserForm /></ProtectedRoute>} />
           <Route path="/create-announcement" element={<ProtectedRoute><CreateAnouncementForm /></ProtectedRoute>} />
           <Route path="/create-room" element={<ProtectedRoute><CreateRoomForm /></ProtectedRoute>} />
